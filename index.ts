3320b7f427a1d71c9bdbe8da10cca79a5d52e666
MAC <michaelcapera@gmail.com>
2024-09-13 20:42:52 -0500

    Testing ts :test_tube:

---
+++ index.ts
@@ -0,0 +1,16 @@
+type Food = string;
+
+
+let pizza: Food = 'Hawaiiana';
+
+type Person = {
+    name: string;
+    age: number;
+    isAgent: boolean;
+}
+
+const person1:Person = {
+    name: 'User1',
+    age: 44,
+    isAgent: true
+}
